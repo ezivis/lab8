e2959e89d2e5f0c801540ef6ceba370c75d79f77
ezivis <49287721+ezivis@users.noreply.github.com>
2020-02-27 09:35:50 -0800

    Add files via upload

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
 	// add your code here
+    L.mapquest.key = 'E1ef9ws8AKs1zH0aoFyfmaZOCAmQu3Je';
+
+    // 'map' refers to a <div> element with the ID map
+    var map = L.mapquest.map('map', {
+      center: [32.88, -117.236],
+      layers: L.mapquest.tileLayer('map'),
+      zoom: 12
+    });
+
+    L.marker([32.88, -117.236]).addTo(map);
 }
